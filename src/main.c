d6533df27ddc428230526c788a51a1540a4e0d77
1 <2>
2021-10-24 23:41:33 +0700

    correct work with memory, add records to file from console parameter

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "memory.h"
 
 typedef struct date {
@@ -19,24 +20,36 @@ typedef struct word {
 
 Word *readFromFile(FILE *file);
 
-int main(void) {
+int main(int argc, char **argv) {
     Word *w;
-    int wasErrors;
-    int d2;
-    // FILE *file = fopen("1.txt", "rt");
-    // if (file) {
-    //     while (!feof(file)) {
-    //         w = readFromFile(file);
-    //         printf("%s %s %s %d.%d.%d %d {%d}\n",
-    //         w->foreign, w->transcription, w->translate,
-    //         w->date.day, w->date.month, w->date.year, w->wasErrors, feof(file));
-    //     }
-    //     fclose(file);
-    // }
-    int* a = (int*)malloc_(sizeof(int));
-    *a = 555;
-    printf("%d\n", *a);
-    free(a);
+
+    if (!strcmp(argv[1], "-add")) {
+        if (argc == 5) {
+            FILE *file = fopen("1.txt", "at");
+            if (file) {
+                time_t currentTime = time(NULL);
+                struct tm *localTime = localtime(&currentTime);
+                fprintf(file, "\n%s %s %s %d.%d.%d %d",
+                    argv[2], argv[3], argv[4],
+                    localTime->tm_mday, localTime->tm_mon + 1, localTime->tm_year - 100, 1);
+                fclose(file);
+            }
+        } else {
+            printf("incorrect using option \"-add\"\n");
+        }
+    }
+
+    FILE *file = fopen("1.txt", "rt");
+    if (file) {
+        while (!feof(file)) {
+            w = readFromFile(file);
+            printf("%s %s %s %d.%d.%d %d {%d}\n",
+            w->foreign, w->transcription, w->translate,
+            w->date.day, w->date.month, w->date.year, w->wasErrors, feof(file));
+        }
+        fclose(file);
+    }
+
     freeAll();
     return 0;
 }
--- src/memory.c
+++ src/memory.c
@@ -5,23 +5,42 @@
 
 #define MALLOC_ATTEMPTS_COUNT 10
 
-Stack static *allocatedMemoryStack = NULL;
+static Stack *allocatedMemoryStack = NULL;
 
-void static *addPointerToAllocatedMemoryStack(void *pointer) {
+static void printAllocatedMemoryStack() {
+    Stack *currentNode = allocatedMemoryStack;
+    printf("> allocated memory stack:\n");
+    printf("**********\n");
+    while (currentNode) {
+        printf("    node:       %p\n", currentNode);
+        printf("    payload:    %p\n", currentNode->payload);
+        printf("    next:       %p\n", currentNode->next);
+        printf("    ----------\n");
+        currentNode = currentNode->next;
+    }
+    printf("**********\n");
+}
+
+static void *addPointerToAllocatedMemoryStack(void *pointer) {
+        #ifdef DEBUG
+        printf("ADD POINTER {%p} to allocated memory stack\n", pointer);
+        printAllocatedMemoryStack();
+        #endif
     Stack *newAllocatedMemoryNode = NULL;
     for(int i = 0; i < MALLOC_ATTEMPTS_COUNT && !newAllocatedMemoryNode; i++) {
         newAllocatedMemoryNode = malloc(sizeof(Stack));
-        printf("{%d}\n", i);
-        printf("{%ld}\n", sizeof(Stack));
+            #ifdef DEBUG
+            printf("allocate memory for new allocated memory stack node attempt № %d\n", i);
+            #endif
     }
     if (newAllocatedMemoryNode) {
         newAllocatedMemoryNode->payload = pointer;
         newAllocatedMemoryNode->next = allocatedMemoryStack;
-        printf("{%p}\n", newAllocatedMemoryNode);
-        printf("{%p}\n", newAllocatedMemoryNode->next);
-        printf("{%p}\n", allocatedMemoryStack);
-        // allocatedMemoryStack = newAllocatedMemoryNode;
+        allocatedMemoryStack = newAllocatedMemoryNode;
     }
+        #ifdef DEBUG
+        printAllocatedMemoryStack();
+        #endif
     return newAllocatedMemoryNode;
 }
 
@@ -31,21 +50,44 @@ void free_(void **ptr) {
 }
 
 void *malloc_(size_t size) {
+        #ifdef DEBUG
+        printf("MALLOC_:\n");
+        #endif
     void *result = NULL;
     for(int i = 0; i < MALLOC_ATTEMPTS_COUNT && !result; i++) {
         result = malloc(size);
+            #ifdef DEBUG
+            printf("malloc attempt № %d: result = %p\n", i, result);
+            #endif
     }
-    if(result) {
-        result = addPointerToAllocatedMemoryStack(result);
+    if(!addPointerToAllocatedMemoryStack(result)) {
+        result = NULL;
     }
     return result;
 }
 
 void freeAll() {
-    Stack *temp = allocatedMemoryStack;
+        #ifdef DEBUG
+        printf("FREE ALL:\n");
+        printAllocatedMemoryStack();
+        #endif
     while (allocatedMemoryStack) {
-        allocatedMemoryStack = temp->next;
-        free(temp->payload);
-        free(temp);
+        Stack *currentNode = allocatedMemoryStack;
+            #ifdef DEBUG
+            printf("current node = %p\n", currentNode);
+            #endif
+        allocatedMemoryStack = allocatedMemoryStack->next;
+            #ifdef DEBUG
+            printf("allocatd memory stack = %p\n", allocatedMemoryStack);
+            printf("free(%p)\n", currentNode->payload);
+            #endif
+        free(currentNode->payload);
+            #ifdef DEBUG
+            printf("free(%p)\n", currentNode);
+            #endif
+        free(currentNode);
+            #ifdef DEBUG
+            printAllocatedMemoryStack();
+            #endif
     }
 }
